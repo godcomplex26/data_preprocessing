2927147171c125b3f15d910b1a999ecc9e1007f6
godcomplex26 <godcomplex26@gmail.com>
2024-04-19 16:11:59 +0900

    main 정리

--- main.py
+++ main.py
@@ -2,34 +2,51 @@ from step1_star_to_ip import random_ip
 from step2_ip_to_int import dataset_ip_to_int
 from step3_set_country_code import set_country_code
 from step4_set_gps_by_code import set_gps_data
+from utils import measure_time
 
+step1 = {
+    "file": "./Raw_Data/2018_mal_data.csv",
+    "column": "IP주소"
+    }
+
+step2 = {
+    "file": "./outputs/step1_start_to_ip.csv",
+    "column": "Trans_IP",
+    "new_column": "Trans_IP_as_int",
+    "output": "./outputs/step2_ip_to_int.csv"
+}
+
+step2_1 = {
+    "file": "./Raw_DATA/ipv4.csv",
+    "column":"시작IP",
+    "new_column": "시작IP_as_int",
+    "output": "./outputs/step2-1_ipv4.csv"
+}
+
+step2_2 = {
+    "file": "./outputs/step2-1_ipv4.csv",
+    "column": "끝IP",
+    "new_column": "끝IP_as_int",
+    "output": "./outputs/step2-2_ipv4.csv"
+}
+
+step3 = {
+    "file": "./outputs/step2_ip_to_int.csv",
+    "file_ipv4": "./outputs/step2-2_ipv4.csv"
+}
+
+step4 = {
+    "file": "./outputs/step3_country_code.csv"
+}
+
+@measure_time
+def run_all():
+    random_ip(step1["file"], step1["column"])
+    dataset_ip_to_int(step2["file"], step2["column"], step2["new_column"], step2["output"])
+    dataset_ip_to_int(step2_1["file"], step2_1["column"], step2_1["new_column"], step2_1["output"])
+    dataset_ip_to_int(step2_2["file"], step2_2["column"], step2_2["new_column"], step2_2["output"])
+    set_country_code(step3["file"], step3["file_ipv4"])
+    set_gps_data(step4["file"])
 
 if __name__=="__main__":
-    file = "./Raw_Data/2018_mal_data.csv"
-    column = "IP주소"
-    random_ip(file, column)
-    
-    file = "./outputs/step1_start_to_ip.csv"
-    column = "Trans_IP"
-    new_column = "Trans_IP_as_int"
-    output="./outputs/step2_ip_to_int.csv"
-    dataset_ip_to_int(file, column, new_column, output)
-    
-    file_ipv4 = "./Raw_DATA/ipv4.csv"
-    column_ipv4 = "시작IP"
-    new_col_ipv4 = "시작IP_as_int"
-    output_ipv4 = "./outputs/step2-1_ipv4.csv"
-    dataset_ip_to_int(file_ipv4, column_ipv4, new_col_ipv4, output_ipv4)
-    
-    file_ipv4_2 = output_ipv4
-    column_ipv4_2 = "끝IP"
-    new_col_ipv4_2 = "끝IP_as_int"
-    output_ipv4_2 = "./outputs/step2-2_ipv4.csv"
-    dataset_ip_to_int(file_ipv4_2, column_ipv4_2, new_col_ipv4_2, output_ipv4_2)
-    
-    file = "./outputs/step2_ip_to_int.csv"
-    file_ipv4 = "./outputs/step2-2_ipv4.csv"
-    set_country_code(file, file_ipv4)
-    
-    file = "./outputs/step3_country_code.csv"
-    set_gps_data(file)
+    run_all()
